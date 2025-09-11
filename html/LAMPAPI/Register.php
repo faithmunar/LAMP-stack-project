d3394a2e8d4ada32c46759a1c116b4798c50dffc
Alan Fairman <129026438+MeatBoy21@users.noreply.github.com>
2025-09-11 15:27:07 -0400

    Fixed duped users, checks for blank fields, and already taken usernames

--- html/LAMPAPI/Register.php
+++ html/LAMPAPI/Register.php
@@ -1,10 +1,15 @@
 <?php
 	$inData = getRequestInfo();
-
+	
 	$firstName = $inData["firstName"];
 	$lastName = $inData["lastName"];
 	$login = $inData["login"];
 	$password = $inData["password"];
+
+	if(empty($firstName ) || empty( $lastName ) || empty($login) || empty($password)) {
+		returnWithError("Please fill out all fields");
+		exit();
+	}
 
 	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
 	if ($conn->connect_error) 
@@ -13,9 +18,24 @@
 	} 
 	else
 	{
+		// Check for duplicate username
+		$stmt = $conn->prepare("SELECT ID FROM Users WHERE Login=?");
+		$stmt->bind_param("s", $login);
+		$stmt->execute();
+		$result = $stmt->get_result();
+		
+		// Cancel registration if login name has been taken
+		if ($result->num_rows > 0) {
+			returnWithError("That login has been taken");
+			$stmt->close();
+			$conn->close();
+			$result->free(); 
+			exit();
+		}
+		$result->free(); 
+
 		$stmt = $conn->prepare("INSERT into Users (FirstName,LastName,Login,Password) VALUES(?,?,?,?)");
 		$stmt->bind_param("ssss", $firstName,$lastName, $login, $password);
-		$stmt->execute();
 
 		if ($stmt->execute()) {
             returnWithError("User added successfully.");
@@ -25,7 +45,6 @@
 
 		$stmt->close();
 		$conn->close();
-		returnWithError("");
 	}
 
 	function getRequestInfo()
@@ -46,3 +65,4 @@
 	}
 	
 ?>
+
