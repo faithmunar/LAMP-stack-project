36aa5209df0c9e18d3fd686ce5b0d283c01513e1
gabe <gabe@debian-inspiron.debian-inspiron>
2025-09-11 16:14:04 -0400

    Added test data to sql table creation

--- html/createtables.sql
+++ html/createtables.sql
@@ -32,12 +32,19 @@ FOREIGN KEY (`UserID`) REFERENCES Users(`ID`)
 
 INSERT INTO Users(FirstName,LastName,Login,Password)
 	VALUES ('Firstname','Lastname','admin','admin123');
+INSERT INTO Users(FirstName,LastName,Login,Password)
+	VALUES ('Secondary','Second','admin2','admin123');
 	
 	
 	
 INSERT INTO Contacts(FirstName,LastName,Phone,Email,UserID)
-	VALUES ('Adam','McAdam','123456789','adam@adam.com',1);
+	VALUES ('Adam','McAdam','1234567890','adam@adam.com',1);
 INSERT INTO Contacts(FirstName,LastName,Phone,Email,UserID)
-	VALUES ('Beth','Bethington','234567890','beth@beth.gov',1);
+	VALUES ('Beth','Bethington','2345678901','beth@beth.gov',1);
 INSERT INTO Contacts(FirstName,LastName,Phone,Email,UserID)
-	VALUES ('Charlie','Charles','345678901','charles@charles.uk',1);
+	VALUES ('Charlie','Charles','3456789012','charles@charles.uk',1);
+
+INSERT INTO Contacts(FirstName,LastName,Phone,Email,UserID)
+	VALUES ('Debbie','Little','3456789012','deb@us.mil',2);
+INSERT INTO Contacts(FirstName,LastName,Phone,Email,UserID)
+	VALUES ('Eddie','Van Halen','4567890123','vanhalen@gmail.com',2);
