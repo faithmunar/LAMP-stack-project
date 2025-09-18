b4525e1617d4bc78c26135072d051dd1da20d2ac
Alan Fairman <129026438+MeatBoy21@users.noreply.github.com>
2025-09-18 15:30:00 -0400

    First pass at deleteContact and editContact

---
+++ html/LAMPAPI/editContact.php
@@ -0,0 +1,52 @@
+<?php
+
+	$inData = getRequestInfo();
+	
+    $firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
+	$phone = $inData["phone"];
+	$email = $inData["email"];
+	$editID = $inData["contactId"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithMessage( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("UPDATE Contacts SET FirstName=?, LastName=?, Phone=?, Email=? where ID=?");
+		$stmt->bind_param("sssss",$firstName,$lastName, $phone, $email, $editID); 
+
+        if($stmt->execute() === TRUE)
+        {
+            if($stmt->affected_rows > 1)
+                returnWithMessage("More than one entry edited");
+            else
+                returnWithMessage("Edit Successful");
+        }
+        else
+            returnWithMessage("Contact not found");
+		
+		$stmt->close();
+		$conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithMessage($message)
+	{
+		$retValue = '{"error":'. $message .'}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
